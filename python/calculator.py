a09738e434061a0a6133ba52d815b0e18dafbc12
evie309 <2014hsbc@gmail.com>
2020-11-09 10:42:47 +0000

    calculator.py sum of integers

--- python/calculator.py
+++ python/calculator.py
@@ -1,1 +1,17 @@
-print("Hello from evie")
+def sum(a,b):
+
+    return int(a)+int(b)
+
+x = input("Enter first number:")
+y = input ("Enter second number:")
+
+if(x.isdigit() and y.isdigit()):
+    sum = sum(x,y)
+    print("sum=",sum)
+else:
+    print("Inputs are not integers")
+
+
+
+
+    
