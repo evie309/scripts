b7b0b9124c15fcf8a3e4194e3e0973cf645234d5
evie309 <2014hsbc@gmail.com>
2020-11-11 11:53:37 +0000

    Subtraction of numbers modified

--- python/calculator.py
+++ python/calculator.py
@@ -6,7 +6,7 @@ def sum(a,b):
 
     return float(a)+float(b)
 
-def diff(a,b):
+def sub(a,b):
     if a>b:
         return float(a)-float(b)
     else:
@@ -47,7 +47,7 @@ if options.o == 'add':
     print("RESULT: ",sum(a,b))
 elif options.o == 'sub':
     a,b = ask_user()
-    print("RESULT: ",diff(a,b))
+    print("RESULT: ",sub(a,b))
     
 def test_sum_int():
 	assert sum(3,2) == 5
